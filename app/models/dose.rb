ff907c70c3048042f0439ea0ee8cba303bad2a5a
Adrien Laudadio <adrien.laudadio.b@gmail.com>
2020-05-14 14:58:30 -0400

    models

---
+++ app/models/dose.rb
@@ -0,0 +1,11 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
+
+
+  # :cocktail_id foreign_key: true
+
+end
